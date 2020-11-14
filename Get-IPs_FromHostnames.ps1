1b1707cb6f1a07a6e6ee9e46ff40fa38692c6b86
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2020-11-13 19:59:54 -0700

    Delete Get-IPs_FromHostnames.ps1

--- Get-IPs_FromHostnames.ps1
+++
@@ -1,4 +0,0 @@
-#ping hostnames through Test-Connection in Powershell, if hosts are reachable, and return IP address
-#Send only 1 ICMP request with -Count 1
-$Hostnames = "PCNAME1", "PCNAME2"
-$Test1 | ForEach{Test-Connection $_ -Count 1}

cd9b382a6377a28cf34d3f07c9bfbe64d364acb0
syntax-eror <46492218+syntax-eror@users.noreply.github.com>
2020-11-13 19:58:16 -0700

    Delete Get-Content_Stream-ZoneIdentifer.ps1

--- Get-Content_Stream-ZoneIdentifer.ps1
+++
@@ -1,3 +0,0 @@
-#Get ZoneIdentifier information for a file, it it exists, to identify files that were downloaded from the internet
-$filename = $null #set this before running
-Get-Content $filename -Stream Zone.Identifier
